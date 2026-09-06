e656c3fd56ff2f943a5b3c073e1e344679bc9912
LeiZ <52287354+Lei-TzuY@users.noreply.github.com>
2026-09-06 22:47:16 +0800

    Format milestone 35 exports

--- src/lib.rs
+++ src/lib.rs
@@ -89,8 +89,9 @@ pub use monitor::{
 };
 pub use monitor_report::{render_bounded_monitor_report, render_monitor_report};
 pub use multi_response::{
-    check_multi_response, check_multi_response_with_limits, check_multi_response_with_product_limits,
-    check_multi_response_with_weak_fairness, check_multi_response_with_weak_fairness_and_limits,
+    check_multi_response, check_multi_response_with_limits,
+    check_multi_response_with_product_limits, check_multi_response_with_weak_fairness,
+    check_multi_response_with_weak_fairness_and_limits,
     check_multi_response_with_weak_fairness_and_product_limits, AnalysisMultiResponseResult,
     BoundedMultiResponseResult, MultiObligationState, MultiResponseCounterexample,
     MultiResponseError, MultiResponseProperty, MultiResponseResult, MultiResponseStatus,
