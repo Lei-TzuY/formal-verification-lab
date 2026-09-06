0586a3e8e5f84f7e6bcbeb0e826e368ee769f307
LeiZ <52287354+Lei-TzuY@users.noreply.github.com>
2026-09-06 20:45:11 +0800

    Export weak-fair response APIs

--- src/lib.rs
+++ src/lib.rs
@@ -121,7 +121,9 @@ pub use reduction::{
     ReductionAudit, ReductionAuditError,
 };
 pub use response::{
-    check_response, check_response_with_limits, AnalysisResponseResult, ObligationState,
+    check_response, check_response_with_limits, check_response_with_weak_fairness,
+    check_response_with_weak_fairness_and_limits,
+    check_response_with_weak_fairness_and_product_limits, AnalysisResponseResult, ObligationState,
     ResponseCounterexample, ResponseError, ResponseProperty, ResponseResult, ResponseStatus,
 };
 pub use response_report::render_response_report;
