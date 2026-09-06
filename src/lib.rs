66fffd8bca66908d403cdb0c99882f28dd4b06ec
LeiZ <52287354+Lei-TzuY@users.noreply.github.com>
2026-09-06 17:49:43 +0800

    Apply rustfmt to fairness exports

--- src/lib.rs
+++ src/lib.rs
@@ -126,8 +126,8 @@ pub use safety::{
 pub use safety_report::render_safety_report;
 pub use temporal::{
     check_action_temporal, check_action_temporal_with_limits,
-    check_action_temporal_with_product_limits, check_action_temporal_with_weak_fairness, ActionAtom,
-    ActionTemporalSpec, AnalysisTemporalResult, BoundedTemporalResult, TemporalBackend,
+    check_action_temporal_with_product_limits, check_action_temporal_with_weak_fairness,
+    ActionAtom, ActionTemporalSpec, AnalysisTemporalResult, BoundedTemporalResult, TemporalBackend,
     TemporalCounterexample, TemporalError, TemporalObligation, TemporalResult, TemporalStatus,
 };
 pub use temporal_parse::{parse_action_temporal, TemporalParseError, TemporalParseErrorKind};
